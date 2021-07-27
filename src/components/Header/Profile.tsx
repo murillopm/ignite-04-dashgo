71630e8dbfd78c35f79de4005789a72e21f64409
Murillo Mesquita <mu.pracucio@gmail.com>
2021-07-27 17:02:57 -0300

    Responsividade na sidebar

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Murillo Mesquita</Text>
-        <Text color="gray.300" fontSize="small">
-          mu.pracucio@gmail.com
-        </Text>
-      </Box>
-
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Murillo Mesquita</Text>
+          <Text color="gray.300" fontSize="small">
+            mu.pracucio@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Murillo Mesquita" src="https://github.com/murillopm.png"/>
     </Flex>
   )
