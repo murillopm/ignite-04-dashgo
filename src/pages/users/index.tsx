cb774fa903ca75ea5dbf18f91ee1541cfd439030
Murillo Mesquita <mu.pracucio@gmail.com>
2021-07-27 17:37:15 -0300

    Responsividade no dashboard e listagem de usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+  
   return (
     <Box>
       <Header />
@@ -29,37 +34,37 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink"/>
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th width="8"></Th>
+                { isWideVersion && <Th>Data de cadastro</Th>}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink"/>
                 </Td>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Box>
                     <Text fontWeight="bold">Murillo Mesquita</Text>
                     <Text fontSize="sm" color="gray.300">mu.pracucio@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>04 de Abril de 2021</Td>
-                <Td>
-                  <Button 
-                    as="a" 
-                    size="sm" 
-                    fontSize="sm" 
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                  >
-                    Editar
-                  </Button>
+                { isWideVersion && <Td>04 de Abril de 2021</Td>}
+              </Tr>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink"/>
                 </Td>
+                <Td px={["4", "4", "6"]}>
+                  <Box>
+                    <Text fontWeight="bold">Murillo Mesquita</Text>
+                    <Text fontSize="sm" color="gray.300">mu.pracucio@gmail.com</Text>
+                  </Box>
+                </Td>
+                { isWideVersion && <Td>04 de Abril de 2021</Td>}
               </Tr>
             </Tbody>
           </Table>
